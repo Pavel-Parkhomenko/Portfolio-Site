9716f730bed9f47d85039625d57d6ed945646b06
Pavel Parkhomenka <pawelparhomenko@gmail.com>
2023-04-04 13:00:38 +0300

    add gradient color

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -3,14 +3,20 @@ import { normalize } from 'styled-normalize'
 
 export default createGlobalStyle`
   ${normalize}
-  
   * {
     font-family: "Roboto", sans-serif;
     font-style: normal;
     transition: background-color 0.4s ease 0.2s;
   }
-  
+
   body, html {
     height: 100vh;
+  }
+  
+  p, span {
+    color: transparent; !important;
+    -webkit-background-clip: text; !important;
+    background-clip: text; !important;
+    background-image: linear-gradient(to right, red, fuchsia); !important;
   }
 `
